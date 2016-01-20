1e980f04b452a50a0d723df77739a2e4ee304fcf
cx <you@example.com>
2016-01-20 10:32:10 +0800

    Initial Commit

---
+++ JingFengMall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JingFengMall
+//
+//  Created by len on 16/1/20.
+//  Copyright (c) 2016年 JingFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
